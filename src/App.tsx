b7258d61c4dffa73d3c0bef5d14294dc975cec5c
Alejandro Frias <alejandrofdiaz@gmail.com>
2020-11-01 09:06:10 +0100

    Adding Meta to web

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Meta } from 'Meta';
 import * as React from 'react';
 
 import { Analytics } from './tracking/Analitics';
 
 export const App = () => (
   <>
+    <Meta />
     <div className="test">Hello World</div>
     <Analytics />
   </>
---
+++ src/Meta.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import Helmet from 'react-helmet';
+
+export const Meta = () => (
+  <Helmet>
+    <title>Elinor Almerara</title>
+  </Helmet>
+);
